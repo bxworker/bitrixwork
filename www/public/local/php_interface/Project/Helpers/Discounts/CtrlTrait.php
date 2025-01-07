545d9a4a53389fadf3aaa2bd2a143fb5d6ee14cf
bxworker <admin@bxworks.ru>
2025-01-08 01:52:41 +0500

    написал пример кастом скидки с ctrl act

--- www/public/local/php_interface/Project/Helpers/Discounts/CtrlTrait.php
+++ www/public/local/php_interface/Project/Helpers/Discounts/CtrlTrait.php
@@ -4,11 +4,36 @@ declare(strict_types=1);
 
 namespace Project\Helpers\Discounts;
 
+use Bitrix\Sale\Internals\SiteCurrencyTable;
 use Project\Helpers\Helpers;
 
 trait CtrlTrait
 {
     protected static string|array|null $cachedControlId = null;
+
+    public static function GetAtomsExFinishing(array $atoms, bool $boolEx): array
+    {
+        return $boolEx
+            ? $atoms
+            : array_map(fn($atom) => $atom['JS'], $atoms);
+    }
+
+    public static function getCurrency(): ?string
+    {
+        if (!static::$boolInit) {
+            return null;
+        }
+
+        if (isset(static::$arInitParams['CURRENCY'])) {
+            return static::$arInitParams['CURRENCY'] ?: null;
+        }
+
+        if (isset(static::$arInitParams['SITE_ID'])) {
+            return SiteCurrencyTable::getSiteCurrency(static::$arInitParams['SITE_ID']) ?: null;
+        }
+
+        return null;
+    }
 
     public static function GetControlID(): string
     {
